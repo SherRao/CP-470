848a9f3c91ede2731cb2ff315ac0937990a47d97
Nausher Rao <sherrao15@gmail.com>
2022-09-25 22:50:11 -0400

    Add 3 activities for part 2

---
+++ app/src/main/java/tech/sherrao/wlu/android/ListItemsActivity.java
@@ -0,0 +1,13 @@
+package tech.sherrao.wlu.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
